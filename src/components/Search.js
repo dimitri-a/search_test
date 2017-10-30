2a02f26a2ae7aad5548b599dcc6ad8ba16a72ec3
dimitric <couwenberg@gmail.com>
2017-10-30 23:00:36 +1100

    search added

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+       this is search
+      </div>
+    );
+  }
+}
+
+export default Search;
