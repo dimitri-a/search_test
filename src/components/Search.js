73596622546f1fda1705244147ee435d8654fdb1
dimitric <couwenberg@gmail.com>
2017-10-31 20:31:25 +1100

    filter working

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,25 +9,53 @@ class Search extends Component {
 
         this.state = {
             data: users,
-            filteredData: users
+            filteredData: users,
+            filter: ''
         }
     }
 
     handleFilter = (event) => {
-        console.log('event',event.target.value);
-        //debugger;
+        console.log('event', event.target.value);
+        debugger;
         if (event) {
 
-            this.setState({ filteredData: this.state.data }, () => {
+            this.setState({ filteredData: this.state.data, filter: event.target.value }, () => {
                 let filtered = this.state.filteredData;
                 if (this.state.search !== '') {
-
-
                     //todo remove
                     //debugger;
                     console.log('event.target.value=', this.refs.search.value);
                     filtered = filtered.filter(person => person.name.includes(this.refs.search.value) > 0);
                 }
+
+                debugger;
+                switch (this.state.filter) {
+                    case 'all':
+                    console.log('alls');
+
+                        break;
+
+                    case 'admin':
+                    console.log('admin');
+                        if (this.state.country !== '') {
+                            filtered = filtered.filter(person => person.country === this.state.country)
+                        }
+
+                        break;
+
+                    case 'nonadmin':
+                    console.log('nonadmin');
+                        filtered = filtered.map(person => ({
+                            ...person,
+                            netWorth: (person.netWorth * Number(this.state.currencyFactor))
+                        }))
+                        break;
+
+                    case 'archived':
+                    console.log('archived');
+                    //filtered = _.sortBy(filtered, [this.state.orderBy])
+                }
+
                 this.setState({ filteredData: filtered });
             });
         }
@@ -42,16 +70,16 @@ class Search extends Component {
 
                     <ul>
                         <li>
-                            <button className='btn btnwidth'  onClick={this.handleFilter} value='all users' >All users</button>
+                            <button className='btn btnwidth' onClick={this.handleFilter} value='all' >All users</button>
                         </li>
                         <li>
                             <button className='btn btnwidth' onClick={this.handleFilter} value='admin' >Administrators</button>
                         </li>
                         <li>
-                            <button className='btn btnwidth'  onClick={this.handleFilter} value='nonadmin' >Nonadmin</button>
+                            <button className='btn btnwidth' onClick={this.handleFilter} value='nonadmin' >Nonadmin</button>
                         </li>
                         <li>
-                            <button className='btn btnwidth'  onClick={this.handleFilter} value='archived' >archived</button>
+                            <button className='btn btnwidth' onClick={this.handleFilter} value='archived' >archived</button>
                         </li>
                     </ul>
 
